89cba600f375d674b1b2cc5178dfd8ce0c2a6ee9
NgChien2603 <abulick113@gmail.com>
2023-10-09 13:14:47 +0700

    setting

---
+++ src/components/pricing/Pricing.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./pricing.css";
+
+const Pricing = () => {
+  return <div>pricing</div>;
+};
+
+export default Pricing;
---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./resume.css";
+
+const Resume = () => {
+  return <div>resume</div>;
+};
+
+export default Resume;
